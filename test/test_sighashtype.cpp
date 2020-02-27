897836ca451cae7fb16ed610f69f32374b3affbf
k-matsuzawa <matsuzawa@cryptogarage.co.jp>
2020-02-27 11:44:46 +0900

    test: add test. add npm script.

--- test/test_sighashtype.cpp
+++ test/test_sighashtype.cpp
@@ -16,7 +16,27 @@ TEST(SigHashType, Constructor_GetSigHashFlag) {
 
   EXPECT_NO_THROW(type2 = SigHashType(SigHashAlgorithm::kSigHashNone, true, false));
   EXPECT_EQ(type2.GetSigHashFlag(), 0x82);
+  EXPECT_TRUE(type2.IsAnyoneCanPay());
+  EXPECT_FALSE(type2.IsForkId());
 
   EXPECT_NO_THROW(type = type2);
   EXPECT_EQ(type.GetSigHashFlag(), 0x82);
+}
+
+TEST(SigHashType, SetFromSigHashFlag) {
+  SigHashType type;
+  EXPECT_NO_THROW(type.SetFromSigHashFlag(0x82));
+  EXPECT_EQ(type.GetSigHashAlgorithm(), SigHashAlgorithm::kSigHashNone);
+  EXPECT_TRUE(type.IsAnyoneCanPay());
+  EXPECT_FALSE(type.IsForkId());
+
+  EXPECT_NO_THROW(type.SetFromSigHashFlag(0x41));
+  EXPECT_EQ(type.GetSigHashAlgorithm(), SigHashAlgorithm::kSigHashAll);
+  EXPECT_FALSE(type.IsAnyoneCanPay());
+  EXPECT_TRUE(type.IsForkId());
+
+  EXPECT_NO_THROW(type.SetFromSigHashFlag(0x03));
+  EXPECT_EQ(type.GetSigHashAlgorithm(), SigHashAlgorithm::kSigHashSingle);
+  EXPECT_FALSE(type.IsAnyoneCanPay());
+  EXPECT_FALSE(type.IsForkId());
 }
