0b270f61e566a4d41b4502e6c52c1efef85f0132
k-matsuzawa <matsuzawa@cryptogarage.co.jp>
2021-04-06 16:01:38 +0900

    hotfix: support cmake 3.20, add workflow modify checker.

--- tools/simple_build.bat
+++ tools/simple_build.bat
@@ -5,8 +5,6 @@ if exist "simple_build.bat" (
   cd ..
 )
 
-CALL cmake -S . -B build -G "Visual Studio 16 2019"
-
-CALL cmake -DENABLE_SHARED=on -DENABLE_TESTS=on -DENABLE_ELEMENTS=on -DCMAKE_BUILD_TYPE=Release --build build
+CALL cmake -S . -B build -G "Visual Studio 16 2019" -DENABLE_SHARED=on -DENABLE_TESTS=on -DENABLE_ELEMENTS=on -DCMAKE_BUILD_TYPE=Release
 
 CALL cmake --build build --config Release --parallel 4
--- tools/simple_build.sh
+++ tools/simple_build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 cd `git rev-parse --show-toplevel`
 
-cmake -S . -B build -G "Unix Makefiles"
-
-cmake -DENABLE_SHARED=off -DENABLE_TESTS=on -DENABLE_ELEMENTS=on -DCMAKE_BUILD_TYPE=Release -DTARGET_RPATH=./build/Release --build build
+cmake -S . -B build -G "Unix Makefiles" -DENABLE_SHARED=off -DENABLE_TESTS=on -DENABLE_ELEMENTS=on -DCMAKE_BUILD_TYPE=Release -DTARGET_RPATH=./build/Release
 
 cmake --build build --config Release --parallel 4
